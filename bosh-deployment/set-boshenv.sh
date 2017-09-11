b85622a20b5bb5336ed4ea2ef5c7474c69867ef5
dmutreja <dhiraj.mutreja@oracle.com>
2017-09-10 20:10:02 -0700

    Changed set-boshenv.sh to use the director variables file provisioned on bastion instance

--- bosh-deployment/set-boshenv.sh
+++ bosh-deployment/set-boshenv.sh
@@ -4,4 +4,4 @@ if [ $# -ne 1 ]; then
   echo "Usage $0 <target-env-alias>"
   exit 1
 fi
-bosh alias-env $1 -e $(bosh int my-bmc-vars.yml --path /internal_ip) --ca-cert <(bosh int creds.yml --path /director_ssl/ca)
+bosh alias-env $1 -e $(bosh int $HOME/director-env-vars.yml --path /internal_ip) --ca-cert <(bosh int creds.yml --path /director_ssl/ca)
