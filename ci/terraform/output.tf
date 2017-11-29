f166535ca9b53242a47dc916b274372e6122791a
dmutreja <dhiraj.mutreja@oracle.com>
2017-11-29 14:56:08 +0400

    Added Ruby templates for generating test configuration files   - Template for generating ini file for running ocitests   - Template for generating the variables yaml file used required by bosh create-env to create     BOSH director
    Added task to generate the above configuration files with names of resources created by terraform
    
    Couple of minor fixes in ci configuration:
      -Remove unused variable from download cpi job
      -Use the same env name for terraform apply/destroy tasks
      -Set serial=true for create-director job
    
    Gitignored .terraform folder
    
    Fix vars file name in teardown-director.sh

--- ci/terraform/output.tf
+++ ci/terraform/output.tf
@@ -1,4 +1,6 @@
-/*
+output vcn {
+  value = "${var.director_vcn}"
+}
 output subnet_id {
   value = "${oci_core_subnet.director_subnet.id}"
 
@@ -14,26 +16,31 @@ output ad {
 output subnet_name {
   value = "${oci_core_subnet.director_subnet.display_name}"
 }
+output subnet_cidr {
+  value = "${oci_core_subnet.director_subnet.cidr_block}"
+}
 
 output subnet_gw {
   value = "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 1)}"
 }
 
-output first_ip {
+output subnet_first_ip {
    value = "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 2)}"
 }
-*/
-
+/*
 output director_subnet {
   value = <<EOS
 {
    "subnet_id" : "${oci_core_subnet.director_subnet.id}",
    "compartment_id" : "${oci_core_subnet.director_subnet.compartment_id}",
    "ad" : "${oci_core_subnet.director_subnet.availability_domain}",
+   "vcn":  "${var.director_vcn}",
    "subnet_name" : "${oci_core_subnet.director_subnet.display_name}",
    "subnet_cidr" : "${oci_core_subnet.director_subnet.cidr_block}",
    "subnet_gw" : "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 1)}",
    "director_ip" : "${cidrhost(oci_core_subnet.director_subnet.cidr_block, 2)}"
+
 }
 EOS
 }
+*/
