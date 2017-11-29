adb94ce9a19dbd693571dec2e15900ee2b85251f
bitsarvi <arvind.x.srinivasan@oracle.com>
2017-11-29 09:41:14 +0530

    Add temporary workaround to download the candidate/dev CPI using the OCI client as the S3 client only downloads a max of 5MB

---
+++ ci/tasks/download-cpi.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Create OCI config
+echo "Creating oci config..."
+OCI_DIR="$HOME/.oci"
+OCI_API_KEY="$OCI_DIR/oci_api_key.pem"
+OCI_CONFIG="$OCI_DIR/config"
+
+mkdir -p ${OCI_DIR}
+cat > ${OCI_API_KEY} <<EOF
+${apikey}
+EOF
+
+cat > $OCI_CONFIG <<EOF
+[DEFAULT]
+user=${oracle_user}
+tenancy=${oracle_tenancy}
+region=${oracle_region}
+key_file=$OCI_API_KEY
+fingerprint=${oracle_fingerprint}
+EOF
+
+chmod 600 ${OCI_API_KEY}
+chmod 600 ${OCI_CONFIG}
+
+semver=`cat version-semver/number`
+
+cpi="bosh-oracle-cpi-dev-${semver}.tgz"
+
+# Download CPI
+oci os object get -ns ${oracle_namespace} -bn ${oracle_bucket} --name ${cpi} --file ${cpi}
+cp ${cpi} candidate/
