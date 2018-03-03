86c0d210964870a85a16f491855f77bfd5ac6e4e
dmutreja <dhiraj.mutreja@oracle.com>
2018-03-03 13:36:12 -0800

    - Documented action/Info to fix lint warnings - Fixed typo in Makefile

--- src/github.com/oracle/bosh-oracle-cpi/action/info.go
+++ src/github.com/oracle/bosh-oracle-cpi/action/info.go
@@ -1,15 +1,19 @@
 package action
 
+// Info action handles the info() request
 type Info struct{}
 
 // lightStemcellFormat name
 // Must match the name produced (and stored in the stemcell manifest) by bosh-oracle-light-stemcell-builder
 const lightStemcellFormat string = "oracle-light"
 
+//NewInfo creates a new Info instance
 func NewInfo() Info {
 	return Info{}
 }
 
+// Run returns information about the stemcell(s)
+// supported by this CPI.
 func (i Info) Run() (StemcellInfo, error) {
 
 	return StemcellInfo{
